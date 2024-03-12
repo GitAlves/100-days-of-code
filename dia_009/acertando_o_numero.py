4b0be64ad5044bd3db50cf3a1200ece99974d08b
Mateus Alves <97408455+GitAlves@users.noreply.github.com>
2024-03-11 22:32:15 -0300

    nineth project

---
+++ dia_009/acertando_o_numero.py
@@ -0,0 +1,24 @@
+from random import randint
+
+tentativas = 4
+numero = randint(1, 10)
+while tentativas:
+
+    numero_jogador = int(input(
+        'Insira um número de 1 a 10: '
+    ))
+
+    if numero_jogador > numero:
+        print(f'Dica: o número é menor que {numero_jogador}.')
+    else:
+        print(f'O número é maior que {numero_jogador}')
+
+    if numero_jogador == numero:
+        print(f'\n{numero_jogador} era o número certo. Parabéns!')
+        break
+
+    tentativas -= 1
+
+if tentativas == 0 and numero_jogador != numero:
+    print('\nAs suas chances acabaram!')
+    print(f'O número sorteado foi {numero}')
