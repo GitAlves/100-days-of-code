23628ef6a14d206d67ccc1486b11ce52fba930cd
Mateus Alves <mateusalvesj3050@gmail.com>
2024-03-17 20:25:22 -0300

    fixing the date interval

---
+++ dia_011_a_013/jogo_da_forca.py
@@ -0,0 +1,44 @@
+from random import choice as ch
+from palavras import lista_frutas
+
+
+def lembrete_tentativas(numero):
+    return f'\n--------- TENTATIVAS: {numero} -------------\n'
+
+
+palavra = ch(lista_frutas)
+palavra_escondida = '_' * len(palavra)
+
+tentativas = 10
+print(lembrete_tentativas(tentativas))
+
+while tentativas:
+    print(palavra_escondida)
+
+    chute = input('\nChute uma letra: ')
+
+    if chute in palavra:
+        print('\nLETRA ENCONTRADA!!!\n')
+
+        rascunho = list(palavra_escondida)
+
+        for posicao, letra in enumerate(palavra):
+            if letra == chute:
+                rascunho[posicao] = letra
+
+        palavra_escondida = ''.join(rascunho)
+    else:
+        tentativas -= 1
+<<<<<<< HEAD
+        print('Errou!')
+=======
+        print('\nErrou!')
+>>>>>>> 7a096fd4722732d439d660a7fc8bd46896a17361
+        print(lembrete_tentativas(tentativas))
+
+    if palavra == palavra_escondida:
+        print('Meus parabéns! Você ganhou.')
+        break
+
+if tentativas == 0:
+    print(f'Game over!\nA palavra era "{palavra}".')
