bc11aa71f3584f7294c5a322daefaaabf5085324
Mateus Alves <mateusalvesj3050@gmail.com>
2024-03-18 22:08:41 -0300

    Creating the path to add new patients

--- dia_015/lista_encadeada.py
+++ dia_015/lista_encadeada.py
@@ -1,3 +1,6 @@
+from time import sleep
+
+
 class UmPaciente:
     def __init__(self, paciente):
         self.paciente = paciente
@@ -7,5 +10,34 @@ class UmPaciente:
         return '%s -> %s' % (self.paciente, self.proximoPaciente)
 
 
-lista_encadeada = UmPaciente('Marcos')
-print(lista_encadeada)
+class ListaPacientes(UmPaciente):
+    def __init__(self):
+        self.paciente = None
+
+    def __repr__(self):
+        return '[' + str(self.paciente) + ']'
+
+    def adicionar_paciente(self, paciente):
+        novoPaciente = UmPaciente(paciente)
+        novoPaciente.proximoPaciente = self.paciente
+        self.paciente = novoPaciente
+
+        print(f'\nAdicionando {paciente} a fila!\n')
+
+
+lista_encadeada = ListaPacientes()
+while True:
+    nome_paciente = input('\nDigite o nome do paciente: ')
+
+    if nome_paciente != '':
+        lista_encadeada.adicionar_paciente(nome_paciente)
+        sleep(2)
+
+        print('\n--------- Listando pacientes -----------\n')
+        print(lista_encadeada)
+    else:
+        break
+
+print('\n\n------- PROCESSO ENCERRADO --------\n\n')
+sleep(2)
+print(f'\nA lista final foi: {lista_encadeada}\n')
