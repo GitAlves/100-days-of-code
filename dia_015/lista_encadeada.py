28b2aa18419eea88dd565fa0de588c12ce1995fe
Mateus Alves <mateusalvesj3050@gmail.com>
2024-03-21 22:19:57 -0300

    Creating the path to add patients in the first position

--- dia_015/lista_encadeada.py
+++ dia_015/lista_encadeada.py
@@ -22,7 +22,15 @@ class ListaPacientes(UmPaciente):
         print(f'\nAdicionando {paciente} a fila!\n')
 
     def adicionar_paciente_no_fim(self, paciente):
-        ...
+        lista = self.paciente
+        novo_paciente = UmPaciente(paciente)
+
+        while lista.paciente is not None:
+            if lista.proximoPaciente is None:
+                lista.proximoPaciente = novo_paciente
+                break
+
+            lista = lista.proximoPaciente
 
     def remover_paciente(self, paciente):
         lista_apoio = self.paciente
@@ -31,3 +39,15 @@ class ListaPacientes(UmPaciente):
             self.paciente = lista_apoio.proximoPaciente
 
         print(f'\nPaciente {paciente} removido da fila!\n')
+
+
+lista_pacientes = ListaPacientes()
+lista_pacientes.adicionar_paciente('Joaquim')
+lista_pacientes.adicionar_paciente('Ana')
+lista_pacientes.adicionar_paciente('Joanna')
+print(f'Listando: {lista_pacientes}')
+lista_pacientes.adicionar_paciente_no_fim('Adamastor')
+lista_pacientes.adicionar_paciente_no_fim('Joaquina')
+print(f'Listando: {lista_pacientes}')
+lista_pacientes.adicionar_paciente_no_fim('Marcos')
+print(f'Listando: {lista_pacientes}')
