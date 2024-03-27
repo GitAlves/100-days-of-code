fda08bb4536cfdaf96818afb3a0bd910dff71dbf
Mateus Alves <mateusalvesj3050@gmail.com>
2024-03-26 22:09:58 -0300

    Creating a basic structure to insert itens in the list

--- dia_023/lista_duplamente_encadeada.py
+++ dia_023/lista_duplamente_encadeada.py
@@ -11,5 +11,22 @@ class Produto:
             self.proximo)
 
 
-no_do_produto = Produto('Carne')
-print(no_do_produto)
+class ListaProdutos(Produto):
+    def __init__(self):
+        self.produto = None
+
+    def __repr__(self):
+        return '[' + str(self.produto) + ']'
+
+    def inserir_produto_no_inicio(self, produto):
+        if self.produto is None:
+            novo_produto = Produto(produto)
+            novo_produto.proximo = self.produto
+            self.produto = novo_produto
+        else:
+            novo_produto = Produto(produto)
+            lista_produtos = self.produto
+            novo_produto.proximo = lista_produtos
+            self.produto = novo_produto
+            print(novo_produto)
+            print(lista_produtos)
