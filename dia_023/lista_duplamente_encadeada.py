3c99fac22d41d7fa05d5e7bbb820023b1d4c9373
Mateus Alves <mateusalvesj3050@gmail.com>
2024-03-27 21:45:24 -0300

    Remaking the adding method in the beggining in a more readble way

--- dia_023/lista_duplamente_encadeada.py
+++ dia_023/lista_duplamente_encadeada.py
@@ -1,3 +1,6 @@
+from time import sleep
+
+
 class Produto:
     def __init__(self, produto):
         self.anterior = None
@@ -21,12 +24,22 @@ class ListaProdutos(Produto):
     def inserir_produto_no_inicio(self, produto):
         if self.produto is None:
             novo_produto = Produto(produto)
-            novo_produto.proximo = self.produto
             self.produto = novo_produto
         else:
             novo_produto = Produto(produto)
-            lista_produtos = self.produto
-            novo_produto.proximo = lista_produtos
+            novo_produto.proximo = self.produto
             self.produto = novo_produto
-            print(novo_produto)
-            print(lista_produtos)
+
+        print(f'\nProduto "{produto}" foi adicionado com sucesso!\n')
+
+
+lista_produtos = ListaProdutos()
+print('\nInserindo "celular" como o primeiro produto na lista: \n')
+sleep(2)
+lista_produtos.inserir_produto_no_inicio('Celular')
+sleep(2)
+print('\nInserindo "Fone de ouvido " como o segundo produto da lista: \n')
+sleep(2)
+lista_produtos.inserir_produto_no_inicio('Fone bluetooth')
+sleep(2)
+print(f'Por hora, a lista duplamente encadeada está ficando assim: \n\n{lista_produtos}\n\n')  # noqa: E501;
