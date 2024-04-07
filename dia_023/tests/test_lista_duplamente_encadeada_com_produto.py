5451ffe43c00ac3c58914249f02e46094d8b52f2
Mateus Alves <mateusalvesj3050@gmail.com>
2024-04-07 20:44:05 -0300

    Testing the insert at the beggining method

--- dia_023/tests/test_lista_duplamente_encadeada_com_produto.py
+++ dia_023/tests/test_lista_duplamente_encadeada_com_produto.py
@@ -4,7 +4,17 @@ from dia_023.lista_duplamente_encadeada import ListaDuplamenteEncadeada
 
 class TestandoListaComProduto(unittest.TestCase):
     def setUp(self):
-        lista_inicial = ListaDuplamenteEncadeada()
-        lista_inicial.adicionar_produto_a_lista_vazia('Celular')
+        self.lista_inicial = ListaDuplamenteEncadeada()
+        self.lista_inicial.adicionar_produto_a_lista_vazia('Celular')
 
-    def testando_
+    def testando_se_o_produto_adicionado_vira_o_primeiro_da_lista(self):
+        self.lista_inicial.adicionar_produto_ao_inicio_da_lista('Fone')
+        self.assertIn('Fone', str(self.lista_inicial.inicio))
+
+    def testando_se_o_tamanho_da_lista_vira_dois_quando_um_segundo_produto_e_adicionado_a_lista(self):  # noqa: E501;
+        self.lista_inicial.adicionar_produto_ao_inicio_da_lista('Fone')
+        self.assertIs(len(self.lista_inicial), 2)
+
+    def testando_se_a_cauda_da_lista_e_igual_a_sua_cabeca_apos_adicionar_um_produto_ao_inicio_da_lista(self):  # noqa: E501;
+        self.lista_inicial.adicionar_produto_ao_inicio_da_lista('Fone')
+        self.assertIs(self.lista_inicial.inicio, self.lista_inicial.fim)
