33e86b683977ddadc27007ac92bb3a9bf2120aaf
Mateus Alves <mateusalvesj3050@gmail.com>
2024-04-08 22:38:49 -0300

    Testing the insert at the end method

--- dia_023/tests/test_lista_duplamente_encadeada_com_produto.py
+++ dia_023/tests/test_lista_duplamente_encadeada_com_produto.py
@@ -9,7 +9,7 @@ class TestandoListaComProduto(unittest.TestCase):
 
     def testando_se_o_produto_adicionado_vira_o_primeiro_da_lista(self):
         self.lista_inicial.adicionar_produto_ao_inicio_da_lista('Fone')
-        self.assertIn('Fone', str(self.lista_inicial.inicio))
+        self.assertEqual('None <-> Fone <-> None <-> Celular <-> None', str(self.lista_inicial.inicio))  # noqa: E501;
 
     def testando_se_o_tamanho_da_lista_vira_dois_quando_um_segundo_produto_e_adicionado_a_lista(self):  # noqa: E501;
         self.lista_inicial.adicionar_produto_ao_inicio_da_lista('Fone')
@@ -18,3 +18,15 @@ class TestandoListaComProduto(unittest.TestCase):
     def testando_se_a_cauda_da_lista_e_igual_a_sua_cabeca_apos_adicionar_um_produto_ao_inicio_da_lista(self):  # noqa: E501;
         self.lista_inicial.adicionar_produto_ao_inicio_da_lista('Fone')
         self.assertIs(self.lista_inicial.inicio, self.lista_inicial.fim)
+
+    def testando_se_o_produto_adicionado_vira_o_ultimo_da_lista(self):
+        self.lista_inicial.adicionar_produto_ao_fim_da_lista('Fone')
+        self.assertEqual('None <-> Celular <-> None <-> Fone <-> None', str(self.lista_inicial.fim))  # noqa: E501;
+
+    def testando_se_o_tamanho_da_lista_vira_dois_quando_um_produto_e_adicionado_ao_seu_fim(self):  # noqa: E501;
+        self.lista_inicial.adicionar_produto_ao_fim_da_lista('Fone')
+        self.assertIs(len(self.lista_inicial), 2)
+
+    def testando_se_a_cabeca_da_lista_e_igual_ao_seu_cauda_apos_adicionar_um_produto_ao_fim_da_lista(self):  # noqa: E501;
+        self.lista_inicial.adicionar_produto_ao_fim_da_lista('Fone')
+        self.assertIs(self.lista_inicial.fim, self.lista_inicial.inicio)  # noqa: E501;
