c88254c342f508a3c0e3c47aa10585a380792016
Mateus Alves <mateusalvesj3050@gmail.com>
2024-04-22 21:58:51 -0300

    Creating the apresentation method

--- dia_049/sistema_de_pedidos_linked_list.py
+++ dia_049/sistema_de_pedidos_linked_list.py
@@ -33,9 +33,26 @@ class FilaDePedidos:
                     break
                 fila = fila.proximo_pedido
 
+        print(f'\n\n{cliente} pediu "{itens}"!\n\n')
+
     def remover_primeiro_pedido(self):
         if self.pedido is None:
-            print('Sem pedidos na fila. Bora descansar ;)')
+            return 'Sem pedidos na fila. Bora descansar ;)\n\n'
         else:
             fila = self.pedido
+
+            print(f'\n\n{fila.nome_cliente} deve seu pedido atendido!\n\n')
+
             self.pedido = fila.proximo_pedido
+
+    def listar_pedidos(self):
+        if self.pedido is None:
+            print('A fila está sem pedidos!')
+        else:
+            print('Código     Nome       Pedido         Preço (R$)\n')
+            fila = self.pedido
+
+            while fila:
+                print(f'{fila.numero_pedido:9} {fila.nome_cliente:11} {fila.itens_pedido:16} {fila.valor}\n')  # noqa: E501;
+
+                fila = fila.proximo_pedido
