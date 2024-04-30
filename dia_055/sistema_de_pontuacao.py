0d415de559dbf9c64f221d8bf6801f98412db380
Mateus Alves <mateusalvesj3050@gmail.com>
2024-04-29 22:03:54 -0300

    Updating 'score update' method

--- dia_055/sistema_de_pontuacao.py
+++ dia_055/sistema_de_pontuacao.py
@@ -5,6 +5,9 @@ class Jogo:
     def __repr__(self):
         return str(self._jogadores)
 
+    def apresentar_dados_jogador(self, jogador):
+        return self._jogadores[jogador]
+
     def adicionar_novo_jogador(self, nome, pontos):
         if len(self._jogadores) == 0:
             self._jogadores["jogador1"] = {'nome': nome, 'pontos': pontos}
@@ -13,10 +16,12 @@ class Jogo:
                 "jogador" + str(len(self._jogadores) + 1)
                 ] = {'nome': nome, 'pontos': pontos}
 
-    def atualizar_pontuacao(self, jogador, nova_pontuacao):
-        if self._jogadores is not None:
-            for chave in self._jogadores.values():
-                if chave['nome'] == jogador:
-                    chave['pontos'] = nova_pontuacao
-        else:
+    def atualizar_pontuacao(self, nome_jogador, nova_pontuacao):
+        if nome_jogador in str(self._jogadores):
+            for jogador in self._jogadores.values():
+                if jogador['nome'] == nome_jogador:
+                    jogador['pontos'] = nova_pontuacao
+        elif self._jogadores is None:
             print('Não há jogadores cadastrados no sistema!')
+        else:
+            print('Este jogador ainda não foi cadastrado!')
