1985db6a3011ba0253ffb312e2344da1fd7a4324
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-07 23:04:32 -0300

    Creating the search methods

--- dia_062/gerenciamento_de_estoque.py
+++ dia_062/gerenciamento_de_estoque.py
@@ -42,11 +42,25 @@ class Raiz:
             no.produto.nome = produto.nome
             no.produto.quantidade = produto.quantidade
 
+    def buscar_item(self, codigo):
+        if self.raiz is None:
+            print('A árvore está vazia! Não há o que mostrar.')
+        else:
+            return self._buscar_item_recursivo(codigo, self.raiz)
+
+    def _buscar_item_recursivo(self, codigo, no):
+        if no is None or no.produto.id == codigo:
+            print(f'ID {codigo}: "{no.produto.nome} com {no.produto.quantidade} unidades" encontrado')  # noqa: E501;
+        elif codigo < no.produto.id:
+            self._buscar_item(codigo, no.esquerdo)
+        elif codigo > no.produto.id:
+            self._buscar_item(codigo, no.direito)
+
 
 produto1 = Produto(1, 'Ruffles', 50)
 produto2 = Produto(0, 'Pringles', 43)
 produto3 = Produto(2, 'Fandangos', 38)
-produto4 = Produto(1, 'Ruffles', 52)
+produto4 = Produto(1, 'Ruffles', 41)
 
 arvore = Raiz()
 arvore.inserir(produto1)
