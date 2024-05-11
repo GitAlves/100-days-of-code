a6e25f65fef79f80dc861ea96b780db5fe0bc67c
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-10 21:11:07 -0300

    Creating the fuction first version

--- dia_068/fibonacci_recursivo.py
+++ dia_068/fibonacci_recursivo.py
@@ -1,15 +1,13 @@
-anterior = 0
-atual = 1
-auxiliar = 0
-vezes = 5  # Os 5 primeiros números da sequência de Fibonacci serão mostrados
+def chamar_fibonacci(vezes):
+    anterior = 0
+    atual = 1
+    auxiliar = 0
 
-while vezes:
-    print(atual, end=' ')
+    while vezes:
+        print(atual, end=' ')
 
-    auxiliar = anterior + atual
-    anterior = atual
-    atual = auxiliar
+        auxiliar = anterior + atual
+        anterior = atual
+        atual = auxiliar
 
-    vezes -= 1
-
-print('\nIsso é tudo por hoje pessoal!')
+        vezes -= 1
