c73a98e4d0f03a4698790f130d8eb1a6c2d4dee8
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-12 22:17:01 -0300

    Fixing the quit program interface

--- dia_068/fibonacci_recursivo.py
+++ dia_068/fibonacci_recursivo.py
@@ -1,3 +1,6 @@
+from termcolor import colored
+
+
 def chamar_fibonacci(vezes, anterior=0, atual=1):
     print(atual, end=' ')
 
@@ -11,13 +14,21 @@ def chamar_fibonacci(vezes, anterior=0, atual=1):
 
 resposta = 'N'
 while resposta != 'S':
-    quant_vezes = int(input('\n\nDigite a quantidade de números da sequência de Fibonacci que você quer ver: '))  # noqa: E501;
+    try:
+        quant_vezes = int(input('\n\nDigite a quantidade de números da sequência de Fibonacci que você quer ver: '))  # noqa: E501;
+    except ValueError:
+        print(colored('\n\nApenas números inteiros são permitidos!\n', 'yellow'))  # noqa: E501;
+    else:
+        print('\n\n')
+        chamar_fibonacci(quant_vezes)
+        print('\n')
 
-    print('\n\n')
-    chamar_fibonacci(quant_vezes)
-    print('\n')
-
-    resposta = input('\n\nGostaria de parar o processo [S / N]: ')
-
+        resposta = input('\n\nGostaria de parar o processo [S / N]: ')
+        while 'S' != resposta != 'N':
+            print('\n\nUse "S" or "N" para prosseguir!')
+            resposta = input('\n\nGostaria de parar o processo [S / N]: ')
+            print(resposta)
+            if 'S' == resposta == 'N':
+                break
 
 print('\n\nFoi um prazer!\n\n')
