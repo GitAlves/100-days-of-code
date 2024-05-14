0965d8ceeb6bc66b16525f847c37ac2587025191
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-13 22:06:39 -0300

    Removing the avoid messages editions

--- dia_068/fibonacci_recursivo.py
+++ dia_068/fibonacci_recursivo.py
@@ -1,6 +1,3 @@
-from termcolor import colored
-
-
 def chamar_fibonacci(vezes, anterior=0, atual=1):
     print(atual, end=' ')
 
@@ -17,15 +14,16 @@ while resposta != 'S':
     try:
         quant_vezes = int(input('\n\nDigite a quantidade de números da sequência de Fibonacci que você quer ver: '))  # noqa: E501;
     except ValueError:
-        print(colored('\n\nApenas números inteiros são permitidos!\n', 'yellow'))  # noqa: E501;
+        print('\n\nApenas números inteiros são permitidos!\n', 'yellow')  # noqa: E501;
     else:
         print('\n\n')
         chamar_fibonacci(quant_vezes)
         print('\n')
 
         resposta = input('\n\nGostaria de parar o processo [S / N]: ')
+
         while 'S' != resposta != 'N':
-            print('\n\nUse "S" or "N" para prosseguir!')
+            print('\n\nUse "S" or "N" para prosseguir!', 'yellow')
             resposta = input('\n\nGostaria de parar o processo [S / N]: ')
             print(resposta)
             if 'S' == resposta == 'N':
