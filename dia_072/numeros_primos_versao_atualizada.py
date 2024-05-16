34ab720a4c232ae942ad34b696c4ed6f2a26ff76
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-15 22:24:37 -0300

    Fixing the function conditional bug

--- dia_072/numeros_primos_versao_atualizada.py
+++ dia_072/numeros_primos_versao_atualizada.py
@@ -1,14 +1,14 @@
 def numero_primo(numero):
+    if numero <= 1:
+        return f'\n{numero} não é um número primo.\n'
     quant_divisores = list(
         filter(
             lambda divisor: numero % divisor == 0,
-            list(
-                range(1, numero + 1)
-            )
+            list(range(1, numero + 1))
         )
     )
 
-    return f'\n{numero} é um número primo!' if len(quant_divisores) == 0 else f'\n{numero} não é um número primo!'  # noqa: E501;
+    return f'\n{numero} é um número primo!' if len(quant_divisores) == 2 else f'\n{numero} não é um número primo.'  # noqa: E501;
 
 
 resposta = 'N'
