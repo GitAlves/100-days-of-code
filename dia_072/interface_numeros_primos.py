c890a25d032ea4ceb6a059bf2254bcd1245f8e96
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-18 20:40:29 -0300

    Creating the label to add the the numbers

--- dia_072/interface_numeros_primos.py
+++ dia_072/interface_numeros_primos.py
@@ -1,8 +1,12 @@
 import tkinter as tk
-# from tkinter import messagebox
-# from numeros_primos_versao_atualizada import numero_primo
 
 root = tk.Tk()
 root.title('Verificador de números primos')
+
+label = tk.Label(root, text='Digite um número:')
+label.pack()
+
+entry = tk.Entry(root)
+entry.pack()
 
 root.mainloop()
