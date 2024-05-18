9313832cc3e864128dded0a9042152803e3e56c8
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-18 20:42:35 -0300

    Adding the buttons in the interface

--- dia_072/interface_numeros_primos.py
+++ dia_072/interface_numeros_primos.py
@@ -9,4 +9,16 @@ label.pack()
 entry = tk.Entry(root)
 entry.pack()
 
+botao_verificar = tk.Button(root, text='Verificar', command=verificar_numero)
+botao_verificar.pack()
+
+botao_primo = tk.Button(root, text='✓', bg='grey', state=tk.DISABLED)
+botao_primo.pack()
+
+botao_nao_primo = tk.Button(root, text='✗', bg='grey', state=tk.DISABLED)
+botao_nao_primo.pack()
+
+novo_numero = tk.Button(root, text='Testar outro número', command=limpar)
+novo_numero.pack()
+
 root.mainloop()
