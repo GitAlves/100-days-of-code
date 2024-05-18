80fd837696317d97c1bb9a497d90e3050b41350c
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-17 22:44:34 -0300

    Creating interface

---
+++ dia_072/interface_numeros_primos.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+# from tkinter import messagebox
+# from numeros_primos_versao_atualizada import numero_primo
+
+root = tk.Tk()
+root.title('Verificador de números primos')
+
+root.mainloop()
