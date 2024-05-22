25629d7d1c13f2e317167defb77b4e3d98ce2f01
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-21 22:01:55 -0300

    Formating the interface with grid styles

--- dia_072/interface_numeros_primos.py
+++ dia_072/interface_numeros_primos.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 from numeros_primos_versao_atualizada import numero_primo
 
 
@@ -8,41 +8,49 @@ def verificar_numero():
     numero_verificado = numero_primo(numero)
 
     if numero_verificado is True:
-        botao_primo.config(bg='green')
-        botao_nao_primo.config(bg='grey')
+        botao_primo.config(style='Certo.TButton')
+        botao_nao_primo.config(style='TButton')
         messagebox.showinfo('Resultado', f'{numero} é um número primo!')
     else:
-        botao_nao_primo.config(bg='red')
-        botao_primo.config(bg='grey')
+        botao_nao_primo.config(style='Errado.TButton')
+        botao_primo.config(style='TButton')
         messagebox.showinfo('Resultado', f'{numero} não é um número primo.')
 
 
 def limpar():
-    botao_primo.config(bg='grey')
-    botao_nao_primo.config(bg='grey')
+    botao_primo.config(style='TButton')
+    botao_nao_primo.config(style='TButton')
     entry.delete(0, tk.END)
 
 
 root = tk.Tk()
 root.title('Verificador de números primos')
-root.config(padx=120, pady=100)
+root.geometry('400x300')
+root.config(bg='yellow')
 
-label = tk.Label(root, text='Digite um número:')
-label.pack()
+style = ttk.Style()
+style.configure('Certo.TButton', background='green')
+style.configure('Errado.TButton', background='red')
 
-entry = tk.Entry(root)
-entry.pack()
+label = ttk.Label(root, text='Digite um número:', font='arial 12', background='yellow')  # noqa: E501;
+label.grid(column=0, row=0, padx=5, pady=30)
 
-botao_verificar = tk.Button(root, text='Verificar', command=verificar_numero)
-botao_verificar.pack()
+entry = ttk.Entry(root, justify='center')
+entry.grid(column=1, row=0, padx=5, pady=30)
 
-botao_primo = tk.Button(root, text='✓', bg='grey', state=tk.DISABLED)
-botao_primo.pack()
+botao_verificar = ttk.Button(root, text='Verificar', command=verificar_numero)
+botao_verificar.grid(column=2, row=0, padx=5, pady=30)
 
-botao_nao_primo = tk.Button(root, text='✗', bg='grey', state=tk.DISABLED)
-botao_nao_primo.pack()
+pergunta_numero_primo = ttk.Label(root, text='Este número é primo?', font='arial 12', background='yellow')  # noqa: E501;
+pergunta_numero_primo.grid(column=1, row=1, padx=5, pady=15)
 
-novo_numero = tk.Button(root, text='Testar outro número', command=limpar)
-novo_numero.pack()
+botao_primo = ttk.Button(root, text='✓', style='TButton', state=tk.DISABLED)
+botao_primo.grid(column=0, row=2, padx=0, pady=0)
+
+botao_nao_primo = ttk.Button(root, text='✗', style='TButton', state=tk.DISABLED)  # noqa: E501;
+botao_nao_primo.grid(column=2, row=2, padx=0, pady=0)
+
+novo_numero = ttk.Button(root, text='Testar outro número', command=limpar)
+novo_numero.grid(column=1, row=3, padx=5, pady=80)
 
 root.mainloop()
