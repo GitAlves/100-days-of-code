a1d7a740e3e24e773899bb5ae5308f3c98659080
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-23 22:06:21 -0300

    Creating a basic user interface

--- dia_081/jogando_dados_atualizado.py
+++ dia_081/jogando_dados_atualizado.py
@@ -5,4 +5,12 @@ def jogar_dado(lados):
     return randint(0, lados)
 
 
-print(jogar_dado(6))
+quant_lados = int(input('\n\nQuantos lados o dado vai ter: '))
+
+resp = 'N'
+while resp != 'S':
+    print(f'\n\nNúmero sorteado: {jogar_dado(quant_lados)}')
+
+    resp = input('Gostaria de encerrar o programa [S/N]: ')
+
+print('\n\nAté mais!\n\n')
