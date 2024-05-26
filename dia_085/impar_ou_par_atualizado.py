62f5840a0a52e2358816e0566d33a4b3374709ec
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-26 17:59:31 -0300

    Adding line breaks

--- dia_085/impar_ou_par_atualizado.py
+++ dia_085/impar_ou_par_atualizado.py
@@ -1,21 +1,21 @@
 def impar_ou_par(numero):
     if numero % 2 == 0:
-        return f'O número {numero} é par!'
+        return f'\n\nO número {numero} é par!'
     else:
-        return f'O número {numero} é impar!'
+        return f'\n\nO número {numero} é impar!'
 
 
 sair = 'n'
 while sair != 'y':
     try:
-        num_inteiro = int(input('Digite um número: '))
+        num_inteiro = int(input('\n\nDigite um número: '))
     except ValueError:
-        print('Insira um número inteiro para ser testado!')
+        print('\n\nInsira um número inteiro para ser testado!')
     else:
         print(impar_ou_par(num_inteiro))
 
         while True:
-            sair = input('Gostaria de encerrar o programa [y/n]: ')
+            sair = input('\n\nGostaria de encerrar o programa [y/n]: ')
 
             match sair:
                 case 'y':
@@ -25,6 +25,6 @@ while sair != 'y':
                     sair = 'n'
                     break
                 case _:
-                    print('Digite [y] para sair ou [n] para continuar!')
+                    print('\n\nDigite [y] para sair ou [n] para continuar!')
 
-print('Foi um prazer!')
+print('\n\nFoi um prazer!\n\n')
