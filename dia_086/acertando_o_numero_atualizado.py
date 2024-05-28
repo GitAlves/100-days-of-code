836d4ba06151b90c37869cee51a5aeea34c11065
Mateus Alves <mateusalvesj3050@gmail.com>
2024-05-27 22:32:38 -0300

    Handling project errors

--- dia_086/acertando_o_numero_atualizado.py
+++ dia_086/acertando_o_numero_atualizado.py
@@ -3,22 +3,25 @@ from random import randint
 tentativas = 4
 numero = randint(1, 10)
 while tentativas:
-    numero_jogador = int(
-        input(
-            'Insira um número entre um e dez: '
+    try:
+        numero_jogador = int(
+            input(
+                'Insira um número entre um e dez: '
+            )
         )
-    )
-
-    if numero_jogador > numero:
-        print(f'Dica: O número é menor que {numero_jogador}.')
+    except ValueError:
+        print('Use números inteiros para tentar acertar o número!')
     else:
-        print(f'O número é maior que {numero_jogador}.')
+        if numero_jogador > numero:
+            print(f'Dica: O número é menor que {numero_jogador}.')
+        else:
+            print(f'O número é maior que {numero_jogador}.')
 
-    if numero_jogador == numero:
-        print(f'\n{numero_jogador} era o número certo. Parabéns!')
-        break
+        if numero_jogador == numero:
+            print(f'\n{numero_jogador} era o número certo. Parabéns!')
+            break
 
-    tentativas -= 1
+        tentativas -= 1
 
 if tentativas == 0 and numero_jogador != numero:
     print('\nAs suas chances acabaram!')
