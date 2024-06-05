24b34fb2e06dda63f8bec396c681daa386daa5ee
Mateus Alves <mateusalvesj3050@gmail.com>
2024-06-04 22:13:50 -0300

    Transforming the main function in a list using map

--- dia_094/tabuada_atualizada.py
+++ dia_094/tabuada_atualizada.py
@@ -1,17 +1,12 @@
-def tabuada(numero, multiplicadores=10):
-    for multiplicador in range(multiplicadores + 1):
-        print(f'{numero} x {multiplicador} = {multiplicador * numero}')
-
-
 numero_escolhido = int(input('Insira um número para ver a sua tabuada: '))
-quantidade = int(
-    input(
-        'Insira até onde você quer saber da tabuada do número: '
+quantidade = range(
+    0, int(
+        input(
+            'Insira até onde você quer saber da tabuada do número: '
+        )
     )
 )
 
-print('\nIniciando tabuada...\n')
+lista_numeros = list(map(lambda x: (numero_escolhido, x, x * numero_escolhido), quantidade))  # noqa: E501;
 
-tabuada(numero_escolhido, quantidade)
-
-print('\nProcesso finalizado!')
+print(lista_numeros)
