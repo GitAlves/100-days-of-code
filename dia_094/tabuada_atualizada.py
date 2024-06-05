6e2072831dddf2acc2ef2571241c957c78d40188
Mateus Alves <mateusalvesj3050@gmail.com>
2024-06-04 22:20:49 -0300

    Creating an interface using while

--- dia_094/tabuada_atualizada.py
+++ dia_094/tabuada_atualizada.py
@@ -1,12 +1,28 @@
-numero_escolhido = int(input('Insira um número para ver a sua tabuada: '))
-quantidade = range(
-    0, int(
-        input(
-            'Insira até onde você quer saber da tabuada do número: '
+escolha = 'N'
+while escolha != 'S':
+    numero_escolhido = int(input('\n\nInsira um número para ver a sua tabuada: '))  # noqa: E501;
+    quantidade = range(
+        0, int(
+            input(
+                '\nInsira até onde você quer saber da tabuada do número: '
+            )
         )
     )
-)
 
-lista_numeros = list(map(lambda x: (numero_escolhido, x, x * numero_escolhido), quantidade))  # noqa: E501;
+    lista_numeros = list(map(lambda x: (numero_escolhido, x, x * numero_escolhido), quantidade))  # noqa: E501;
 
-print(lista_numeros)
+    print(lista_numeros)
+
+    while True:
+        escolha = input('\n\nGostaria de sair [S/N]: ')
+
+        match escolha:
+            case 'S':
+                break
+            case 'N':
+                break
+            case _:
+                print('\n\nDigite [S] para sair ou [N] para continuar vendo tabuadas!')  # noqa: E501;
+
+
+print('\n\nFoi um prazer!\n\n')
