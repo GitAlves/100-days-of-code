db0623ea50ef1484465e7fc30a6594c823f38d95
Mateus Alves <mateusalvesj3050@gmail.com>
2024-06-06 19:05:32 -0300

    Creating an interface for the project using the Tinter library

--- dia_096/ano_bissexto_atualizado.py
+++ dia_096/ano_bissexto_atualizado.py
@@ -1,9 +1,53 @@
+import tkinter as tk
+
+
 def validador(ano: int):
     if ano % 4 == 0:
-        return f'{ano} é um ano bissexto!'
+        return f'\n\n{ano} é um ano bissexto!'
     else:
-        return f'{ano} não é um ano bissexto!'
+        return f'\n\n{ano} não é um ano bissexto!'
 
 
-ano_escolhido = int(input('Insira um ano qualquer: '))
-print(validador(ano_escolhido))
+janela = tk.Tk()
+janela.title('Validador de anos bissextos!')
+janela.geometry('400x250')
+
+texto = tk.Label(
+    text='Insira um ano qualquer: '
+)
+texto.grid(
+    column=0,
+    row=0,
+    padx=5,
+    pady=10
+)
+
+caixa_de_entrada = tk.Entry()
+caixa_de_entrada.grid(
+    column=1,
+    row=0,
+    padx=5,
+    pady=10
+)
+
+botao_verificar = tk.Button(
+    text='Verificar',
+    command=validador
+)
+botao_verificar.grid(
+    column=2,
+    row=0,
+    padx=5,
+    pady=10
+)
+
+texto_resposta = tk.Label(text='O texto vai vir aqui ;)')
+texto_resposta.grid(
+    column=0,
+    row=1,
+    padx=5,
+    pady=10,
+    columnspan=3
+)
+
+janela.mainloop()
