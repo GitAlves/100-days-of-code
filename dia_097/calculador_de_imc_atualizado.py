d4950f3c5b423501352cbfd83e51e71d17b07778
Mateus Alves <mateusalvesj3050@gmail.com>
2024-06-07 11:49:44 -0300

    Creating the interce using the tkinter library

--- dia_097/calculador_de_imc_atualizado.py
+++ dia_097/calculador_de_imc_atualizado.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+
+
 def calcula_imc(peso, altura):
     imc = peso / (altura ** 2)
 
@@ -9,9 +12,73 @@ def calcula_imc(peso, altura):
         return 'com um leve sobrepeso'
     else:
         return 'em um grau de obesidade'
+    
+
+def verificar_valores(peso, altura):
+    ...
 
 
-peso = float(input('Digite o seu peso [Ex.: 56.7]: '))
-altura = float(input('Digite a sua altura em metros [Ex.: 1.54]: '))
+interface = tk.Tk()
+interface.title('Calculador de IMC')
+interface.geometry('400x300')
 
-print(f'Você está {calcula_imc(peso, altura)}')
+texto_peso = tk.Label(
+    text='Insira o seu peso'
+)
+texto_peso.grid(
+    column=0,
+    row=0,
+    padx=5,
+    pady=10
+)
+
+caixa_peso = tk.Entry()
+caixa_peso.grid(
+    column=1,
+    row=0,
+    padx=5,
+    pady=10
+)
+
+texto_altura = tk.Label(
+    text='Insira a sua altura'
+)
+texto_altura.grid(
+    column=0,
+    row=1,
+    padx=5,
+    pady=10
+)
+
+caixa_altura = tk.Entry()
+caixa_altura.grid(
+    column=1,
+    row=1,
+    padx=5,
+    pady=10
+)
+
+botao_verificar = tk.Button(
+    text='Verificar',
+    command=verificar_valores
+)
+botao_verificar.grid(
+    column=0,
+    row=2,
+    padx=5,
+    pady=10,
+    columnspan=2
+)
+
+resultado_imc = tk.Label(
+    text='O resultado IMC virá aqui!'
+)
+resultado_imc.grid(
+    column=0,
+    row=3,
+    padx=5,
+    pady=10,
+    columnspan=2
+)
+
+interface.mainloop()
