1c26721276263dfd78f9681a2ade826655f2c699
Mateus Alves <mateusalvesj3050@gmail.com>
2024-06-08 20:34:41 -0300

    Connecting the difficuties screen with the game screen

--- dia_098/jogo_da_forca_atualizado.py
+++ dia_098/jogo_da_forca_atualizado.py
@@ -51,13 +51,20 @@ if tentativas == 0:
     print(f'Game over!\nA palavra era "{palavra}".')
 
 
+class Jogo():
+    def __init__(self, tentativas):
+        self._tentativas = tentativas
+
+
 def menu_dificuldades():
     menu = tk.Tk()
     menu.title('Escolha a dificuldade')
     menu.geometry('300x250')
 
     botao_facil = tk.Button(
+        menu,
         text='Fácil - Acerte a palavra em 10 tentativas',
+        command=dificuldade_facil
     )
     botao_facil.grid(
         column=0,
@@ -66,11 +73,12 @@ def menu_dificuldades():
         pady=(50, 10)
     )
 
-    botao_medio = tk.Button(
+    botao_normal = tk.Button(
         menu,
-        text='Normal - Acerte a palavra em 6 tentativas'
+        text='Normal - Acerte a palavra em 6 tentativas',
+        command=dificuldade_normal
     )
-    botao_medio.grid(
+    botao_normal.grid(
         column=0,
         row=1,
         padx=5,
@@ -79,7 +87,8 @@ def menu_dificuldades():
 
     botao_dificil = tk.Button(
         menu,
-        text='Difícil - Acerte a palavra em 4 tentativas'
+        text='Difícil - Acerte a palavra em 4 tentativas',
+        command=dificuldade_dificil
     )
     botao_dificil.grid(
         column=0,
@@ -89,6 +98,18 @@ def menu_dificuldades():
     )
 
     menu.mainloop()
+
+
+def dificuldade_facil():
+    jogo = Jogo(10)
+
+
+def dificuldade_normal():
+    jogo = Jogo(6)
+
+
+def dificuldade_dificil():
+    jogo = Jogo(4)
 
 
 menu_dificuldades()
