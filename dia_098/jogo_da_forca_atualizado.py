3cc4b3050dbacbdd00215f8d34472aae1505a2d8
Mateus Alves <mateusalvesj3050@gmail.com>
2024-06-08 21:32:15 -0300

    Creating the game main screen

--- dia_098/jogo_da_forca_atualizado.py
+++ dia_098/jogo_da_forca_atualizado.py
@@ -1,9 +1,80 @@
 import tkinter as tk
+from random import choice as ch
 
 
 class Jogo():
     def __init__(self, tentativas):
         self._tentativas = tentativas
+        self._palavra = ''
+
+    def tela_jogo(self):
+        tela = tk.Tk()
+        tela.title('Acerte a palavra')
+        tela.geometry('600x400')
+
+        texto_tentativas = tk.Label(
+            tela,
+            text=f'Tentativas: {self._tentativas}'
+        )
+        texto_tentativas.grid(
+            column=0,
+            row=0,
+            padx=5,
+            pady=10,
+            columnspan=2
+        )
+
+        texto_letras_erradas = tk.Label(
+            tela,
+            text='Letras erradas: '
+        )
+        texto_letras_erradas.grid(
+            column=0,
+            row=1,
+            padx=5,
+            pady=10,
+            columnspan=2
+        )
+
+        texto_chute = tk.Label(
+            tela,
+            text='Chute uma letra'
+        )
+        texto_chute.grid(
+            column=0,
+            row=2,
+            padx=5,
+            pady=10
+        )
+
+        campo_chute = tk.Entry(
+            tela,
+        )
+        campo_chute.grid(
+            column=1,
+            row=2,
+            padx=5,
+            pady=10
+        )
+
+        botao_verificar = tk.Button(
+            tela,
+            text='Verificar'
+        )
+        botao_verificar.grid(
+            column=0,
+            row=3,
+            padx=5,
+            pady=10,
+            columnspan=2
+        )
+
+        tela.mainloop()
+
+    def sortear_palavra(self):
+        with open('frutas.txt') as arq:
+            rascunho = arq.readlines()
+            self._palavra = ch(rascunho).split('\n')[0].upper()
 
 
 def menu_dificuldades():
@@ -52,14 +123,17 @@ def menu_dificuldades():
 
 def dificuldade_facil():
     jogo = Jogo(10)
+    jogo.tela_jogo()
 
 
 def dificuldade_normal():
     jogo = Jogo(6)
+    jogo.tela_jogo()
 
 
 def dificuldade_dificil():
     jogo = Jogo(4)
+    jogo.tela_jogo()
 
 
-menu_dificuldades()
+menu = menu_dificuldades()
