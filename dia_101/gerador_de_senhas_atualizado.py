9ae12755544ad9269a702da971abb27849d14ecc
Mateus Alves <mateusalvesj3050@gmail.com>
2024-06-11 19:52:01 -0300

    Creating a temporary interface

--- dia_101/gerador_de_senhas_atualizado.py
+++ dia_101/gerador_de_senhas_atualizado.py
@@ -19,12 +19,32 @@ def gerar_senha(quant_caracteres):
     return senha
 
 
-quantidade_caracteres = int(
-    input(
-        'Insira qual a quantidade de caracteres que a senha deverá ter: '
-    )
-)
+quantidade_caracteres = 0
+resp = 'N'
+while resp != 'S':
+    try:
+        quantidade_caracteres = int(
+            input(
+                '\n\nInsira qual a quantidade de caracteres que a senha deverá ter: '  # noqa: E501;
+            )
+        )
+    except ValueError:
+        print('\n\nInsira um número inteiro para definir o tamanho da senha!')
+    else:
+        senha = gerar_senha(quantidade_caracteres)
 
-senha = gerar_senha(quantidade_caracteres)
+        print(f'\n\nA senha sugerida foi: {senha}')
 
-print(f'\nA senha sugerida foi: {senha}')
+        while True:
+            resp = input('\n\nGostaria de sair [S/N]: ')
+
+            match resp:
+                case 'S':
+                    break
+                case 'N':
+                    break
+                case _:
+                    print('\n\nDigite [S] para sair ou [N] para pedir mais uma senha!')  # noqa: E501;
+
+
+print('\n\nFoi um prazer!\n\n')
